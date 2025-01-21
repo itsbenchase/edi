75ebeb299284d2c816af4a764e206b3cff3dc815
itsbenchase <ben.chase@benchase.info>
2025-01-21 13:44:35 -0600

    update 1-21 / 4035

--- code/RouteList.java
+++ code/RouteList.java
@@ -193,7 +193,7 @@ public class RouteList
               data = data.substring(data.indexOf(";") + 1);
               double miles = Double.parseDouble(data.substring(0, data.indexOf(";")));
               data = data.substring(data.indexOf(";") + 1);
-              double edi = Double.parseDouble(data);
+              double edi = Double.parseDouble(data.substring(0, data.indexOf(";")));
   
               lengths.add(miles);
               edis.add(edi);
